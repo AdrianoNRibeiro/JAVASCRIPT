c51174fcf864f8b011038041b8d8251fd0ea13cd
Adriano Ribeiro <94660112+AdrianoNRibeiro@users.noreply.github.com>
2022-02-09 09:23:45 -0300

    diatardenoite01

--- exDiaTardeNoite/script.js
+++ exDiaTardeNoite/script.js
@@ -2,6 +2,20 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 15
     msg.innerHTML = `Agora são ${hora} horas.` 
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'IMAGENS/dia.png'
+        document.body.style.backgroundColor = "yellow"
+    }else if (hora >= 13 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'IMAGENS/tarde.png'
+        document.body.style.backgroundColor = "red"
+    }else {
+        //BOA NOITE
+        img.src = 'IMAGENS/noite.png'
+        document.body.style.backgroundColor = "black"
+    }
 }
