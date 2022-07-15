4fc995b0156a04184b0e264353f728bde4c187c8
Adriano Ribeiro <94660112+AdrianoNRibeiro@users.noreply.github.com>
2022-07-14 21:32:03 -0300

    update hora do dia

--- exDiaTardeNoite/script.js
+++ exDiaTardeNoite/script.js
@@ -2,9 +2,9 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 15
-    msg.innerHTML = `Agora são ${hora} horas.` 
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.` 
     if (hora >= 0 && hora < 12) {
         //BOM DIA
         img.src = 'IMAGENS/dia.png'
